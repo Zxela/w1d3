dcbe145a04325f636ba8c476ef6263729800bf81
Alexander Zvaniga <alex.zvaniga@gmail.com>
2018-07-04 18:46:08 +0000

    cleaned up comments

--- charCounting.js
+++ charCounting.js
@@ -15,27 +15,6 @@ function charCounting(str) {
 //
 console.log(charCounting(str));
 
-// function countLetters(noSpaceStr) {
-//   var runtime = findUniques(splitStr).length // times to run loop (length of split string minus duplicate letters)
-//   for (var i = 0; i < runtime, i++) {
-//     length = findUniques(splitStr)
-//     if (length[i] === splitStr[i]) {
-//       result.findUniques(splitStr)[i] = function (){
-//          var count = str.replace(createGloabalButton(), "").length;
-//          return count;
-//           //looking for "a" and replacing with ""
-//       }
-//     }
-//     console.log("number of remaining letters; " + splitStr.length);
-//   }
-// }
-// //countLetters(str);
-
-
-// function createGlobalButton(Keyv) {
-//   return "/[^" + Keyv + "]/g"
-
-// }
 
 // //leave only unique letters//
 // function findUniques(string){
