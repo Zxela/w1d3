1acc18ee85e1214077097b73d430928ee7f9b825
Alexander Zvaniga <alex.zvaniga@gmail.com>
2018-07-04 18:44:00 +0000

    total revamp - nothing is the same - simplified to 2 for loops

--- charCounting.js
+++ charCounting.js
@@ -1,24 +1,48 @@
 //test code
-var str = "akljsda;k s j d a s ; bkdj";
-//
+var str = "test string"; //test string
 
-
-
-function spaceEraser(str) {
-var noSpaceStr = str.replace(/\s/g, "");
-return noSpaceStr;
-}
-function countLetters(noSpaceStr) {
-  for (var i = 0; i < noSpaceStr.length, i++) {
-    console.log(createGlobalButton(Keyv));
-    var splitStr = str.replace(/[a]/g, ""); //looking for "a" and replacing with ""
-    console.log("number of remaining letters; " + splitStr.length);
+function charCounting(str) {
+  var result = {}//
+  var noSpaceStr = str.replace(/\s/g, "").toLowerCase(); //removing spaces
+  for (var i = 0; i < noSpaceStr.length; i++) { //sets empty array
+     result[noSpaceStr[i]] = 0;
   }
+  for (var i = 0; i < noSpaceStr.length; i++) { // adds 1 to empty array
+    result[noSpaceStr[i]] += 1;
+  }
+  return result;
 }
-//countLetters(str);
+//
+console.log(charCounting(str));
+
+// function countLetters(noSpaceStr) {
+//   var runtime = findUniques(splitStr).length // times to run loop (length of split string minus duplicate letters)
+//   for (var i = 0; i < runtime, i++) {
+//     length = findUniques(splitStr)
+//     if (length[i] === splitStr[i]) {
+//       result.findUniques(splitStr)[i] = function (){
+//          var count = str.replace(createGloabalButton(), "").length;
+//          return count;
+//           //looking for "a" and replacing with ""
+//       }
+//     }
+//     console.log("number of remaining letters; " + splitStr.length);
+//   }
+// }
+// //countLetters(str);
 
 
-function createGlobalButton(Keyv) {
-  return "/[^" + Keyv + "]/g"
+// function createGlobalButton(Keyv) {
+//   return "/[^" + Keyv + "]/g"
 
-}
+// }
+
+// //leave only unique letters//
+// function findUniques(string){
+//     var unique='';
+//     for(var i=0; i<string.length; i++){
+//         if(unique.indexOf(string[i])==-1){
+//             unique += string[i];
+//         }
+//     }
+//     return unique;
